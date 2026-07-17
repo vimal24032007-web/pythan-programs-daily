3b6b449aa4837f13ddbcc51c05fb3f65dd9c6bb2
Vimal raj <vimal24032007@gmail.com>
2026-07-18 01:06:30 +0530

    Create program 13.py

---
+++ program 13.py
@@ -0,0 +1,11 @@
+low, high = 10, 10000
+for n in range(low, high + 1):
+    order = len(str(n))   
+    sum = 0
+    temp = n
+    while temp > 0:
+        digit = temp % 10
+        sum += digit ** order
+        temp //= 10
+    if n == sum:
+        print(n, end=", ")
