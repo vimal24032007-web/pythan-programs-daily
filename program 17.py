ec990a42523b16639a7c005ba14f83d6ad2297c6
Vimal raj <vimal24032007@gmail.com>
2026-07-20 20:15:35 +0530

    Create program 17.py

---
+++ program 17.py
@@ -0,0 +1,17 @@
+def Fibonacci (n):
+    
+    if n < 2:
+        
+        return n
+        
+    fs = [0, 1]
+    
+    for i in range(1, n):
+        
+        fs.append(fs[i] + fs[i - 1])
+        
+    return fs [n]
+    
+n = 6
+
+print (Fibonacci (n-1))
